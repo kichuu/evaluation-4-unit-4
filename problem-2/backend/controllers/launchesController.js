d91af20ed83261c9e229ec9b3d03bf22acc164ac
krishnadev <krishnadevkichus@gmail.com>
2025-03-17 18:59:00 +0530

    minor changes

--- problem-2/backend/controllers/launchesController.js
+++ problem-2/backend/controllers/launchesController.js
@@ -1,61 +1,72 @@
 import mongoose from "mongoose"
 import Launches from "../models/launchesModel.js"
 
-// orbit : {type : String, enum :["LE0" , "MEO" , "HEO"]},
-// launchyear : {type : String},
-// launchsuccess : {type : Boolean},
-// firststagereused : {type : Boolean }
+export const postAllLaunches = async (req, res) => {
+  const response = await fetch("https://api.spacexdata.com/v3/launches")
+  const newdata = await response.json()
 
-export const postLaunches = async (req, res) => {
-  let { orbit, launchyear, launchsuccess, firststagereused } = req.body
-  let newlaunch = {}
-  newlaunch.orbit = orbit
-  ;(newlaunch.launchyear = launchyear),
-    (newlaunch.launchsuccess = launchsuccess),
-    (newlaunch.firststagereused = firststagereused),
-    Launches.create(newlaunch)
-  res.send({ message: "new launchCreated", newlaunch })
+  for (const data of newdata) {
+    const existingLaunch = await Launches.findOne({
+      flightnumber: data.flight_number,
+    })
+
+    if (!existingLaunch) {
+      const newlaunch = {
+        flightnumber: data.flight_number,
+        orbit: data.rocket.second_stage.payloads[0]?.orbit ,
+        launchyear: Number(data.launch_year),
+        launchsuccess: data.launch_success,
+        firststagereused: data.rocket.first_stage.cores[0]?.reused ,
+      }
+      await Launches.create(newlaunch)
+    }
+  }
+
+  res.json({ message: "Data added successfully" })
 }
 
-export const postAllLaunches = async (req, res) => {
-  const alldata = await fetch("https://api.spacexdata.com/v3/launches")
-  let newdata = await alldata.json()
-  newdata.forEach((data) => {
-    let newlaunch = {}
-    newlaunch.flightnumber = data.data
-    newlaunch.orbit = data.rocket.second_stage.payloads[0].orbit
-    ;(newlaunch.launchyear = Number(data.launch_year)),
-      (newlaunch.launchsuccess = data.launch_success),
-      (newlaunch.firststagereused = data.rocket.first_stage.cores[0].reused),
-      Launches.create(newlaunch)
+export const postLaunches = async (req, res) => {
+  const { orbit, launchyear, launchsuccess, firststagereused } = req.body
+
+  const newLaunch = await Launches.create({
+    orbit,
+    launchyear: Number(launchyear),
+    launchsuccess,
+    firststagereused,
   })
-  res.send("data added succesfully")
+
+  res.json({ message: "New launch created", newLaunch })
 }
 
 export const getLaunchesDate = async (req, res) => {
-  let { startdate, enddate } = req.body
+  try {
+    let { startdate, enddate } = req.body
 
-  startdate = Number(startdate)
-  enddate = Number(enddate)
+    startdate = Number(startdate)
+    enddate = Number(enddate)
 
-  const launches = await Launches.find({
-    launchsuccess: true,
-    launchyear: { $gte: startdate, $lte: enddate },
-  })
-    .sort({ launchyear: 1 })
-    .limit(2)
+    const launches = await Launches.find({
+      launchsuccess: true,
+      launchyear: { $gte: startdate, $lte: enddate },
+    })
+      .sort({ launchyear: 1 })
+      .limit(2)
 
-  res.json(launches)
+    res.json(launches)
+  } catch (error) {
+    console.error("Error fetching launches:", error)
+    res.status(500).json({ error: "Internal Server Error" })
+  }
 }
 
 export const getReuse = async (req, res) => {
-  let resued = await Launches.find({ firststagereused: true })
-
-  res.status(201).send({ resuedtimes: resued.length })
+  const reused = await Launches.find({ firststagereused: true })
+  const reusedcount = reused.length
+  res.json({ reusedcount })
 }
 
 export const getOrbit = async (req, res) => {
-  let orbit = req.body.orbit
-  let launches = Launches.find({ orbit: orbit })
-  res.send(launches.length)
-}
+    let {orbit} = req.body
+    let launches = await Launches.find({ orbit: orbit })
+    res.json({orbitcount : launches.length})
+  }
--- problem-2/backend/routes/launchesroutes.js
+++ problem-2/backend/routes/launchesroutes.js
@@ -1,11 +1,18 @@
 import { Router } from "express";
-import { getLaunchesDate, getOrbit, getReuse, postAllLaunches, postLaunches } from "../controllers/launchesController.js";
-const router = Router()
-router.get("/reused" , getReuse)
+import {
+    getLaunchesDate,
+    getOrbit,
+    getReuse,
+    postAllLaunches,
+    postLaunches
+} from "../controllers/launchesController.js";
 
-router.post("/all" ,postAllLaunches)
+const router = Router();
 
-router.post("/date" , getLaunchesDate)
-router.get("/orbit" , getOrbit)
-router.post("/" , postLaunches)
-export default router
+router.get("/reused", getReuse);
+router.post("/all", postAllLaunches);
+router.post("/date", getLaunchesDate);
+router.get("/orbit", getOrbit);
+router.post("/", postLaunches);
+
+export default router;
